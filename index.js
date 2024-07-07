341a805ca3bbe28dfc2e970ffae84704e0d8101c
David Antonio <happywybie@gmail.com>
2024-07-06 23:17:39 -0600

    ci: add search and try catch for database connection

--- index.js
+++ index.js
@@ -18,17 +18,47 @@ async function go() {
 
 go()
 
+// get all
 app.get('/', async (req, res) => {
-    const [users] = await db.execute('SELECT * FROM usuario')
-    console.log(users)                
-    res.send(`<ul>${users.map(user => `<li>${user.nombre}</li>`).join('')}</ul>`)
+
+    try {
+        const [users] = await db.execute('SELECT * FROM usuario')
+        console.log(users)                
+        res.send(`<ul>${users.map(users => `<li>${users.nombre}</li>`).join('')}</ul>`)
+    } catch (error) {
+        console.log(error)
+        res.status(505).send('505 server connection');
+    }
+    
 })
 
-app.post('/', async (req, res) => {
+ // search
+app.get('/search/:id', async (req, res) => {
+    const id = req.params.id;
+    const [post] = await db.execute(`select * from usuario where id = ${id}`)
+
+    try {
+        if (post.length > 0) {
+            console.log(post)              
+          console.log(`Id: ${id}`)                
+            res.send(`<h2>${post.map(post =>post.nombre).join('')}</h2>`)
+          } else {
+            res.status(404).send('Post not found');
+          }
+    } catch (error) {
+        console.log(error)
+        res.status(505).send('505 server connection');
+    }
+
 })
 
-app.post('/', async (req, res) => {
+
+
+app.post('/create', async (req, res) => {
 })
 
-app.delete('/', async (req, res) => {
+app.post('/update', async (req, res) => {
+})
+
+app.delete('/delete', async (req, res) => {
 })
